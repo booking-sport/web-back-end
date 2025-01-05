ddc1252dfe20345d90a8d496f5dcfbbbe447947b
Dat02 <datnguyendinh1311@gmail.com>
2025-01-05 13:11:21 +0700

    fix(auth): read header auth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,115 +1,114 @@
-const jwt = require('jsonwebtoken');
-const { errorHandler } = require('../helpers/errorHandler');
-const orderService = require('../services/orderService');
-const stadiumService = require('../services/stadiumService');
+const jwt = require("jsonwebtoken");
+const { errorHandler } = require("../helpers/errorHandler");
+const orderService = require("../services/orderService");
+const stadiumService = require("../services/stadiumService");
 
-exports.verifyToken = (req,res,next) => {
-    console.log(req.cookies);
-    const token = req.cookies.jwt;
-    if(!token) return next(errorHandler(403, 'token not found'));
+exports.verifyToken = (req, res, next) => {
+  const authorizationHeader = req.headers["authorization"];
+  if (!authorizationHeader) {
+    return res.status(401).json({ message: "Authorization header missing" });
+  }
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRETE, (err, user) => {
-        if(err) return next(errorHandler(403, 'token is invalid'));
-        req.user = user;
-        next();
-    });
-}
+  const token = authorizationHeader.split(" ")[1];
+  if (!token) return next(errorHandler(403, "token not found"));
 
-exports.decodeToken = (req,res,next) => {
-    const token = req.cookie.jwt;
-    if(!token) return next();
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRETE, (err, user) => {
-        if(err) return next(errorHandler(403, 'token is invalid'));
-        req.user = user;
-        next();
-    });
-}
-
-exports.isPlayer = (req,res,next) => {
-    const user = req.user;
-    if(!user) return next(errorHandler(403, 'user not autenicated'));
-    const playerId = user.player_id;
-    if(!playerId) return next(errorHandler(403, 'not player role'));
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRETE, (err, user) => {
+    if (err) return next(errorHandler(403, "token is invalid"));
+    req.user = user;
     next();
-}
+  });
+};
 
+exports.decodeToken = (req, res, next) => {
+  const token = req.cookie.jwt;
+  if (!token) return next();
 
-exports.isAdmin = (req,res,next) => {
-    const adminId = req.user.admin_id;
-    if(!adminId) return next(errorHandler(403, 'not admin system'));
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRETE, (err, user) => {
+    if (err) return next(errorHandler(403, "token is invalid"));
+    req.user = user;
     next();
-}
+  });
+};
 
-exports.isManager = (req,res,next) => {
-    const manager_id = req.user.manager_id;
-    if(!manager_id) return next(errorHandler(403, 'not a stadium manager'));
-    next();
-}
+exports.isPlayer = (req, res, next) => {
+  const user = req.user;
+  if (!user) return next(errorHandler(403, "user not autenicated"));
+  const playerId = user.player_id;
+  if (!playerId) return next(errorHandler(403, "not player role"));
+  next();
+};
 
-exports.isOwner = (req,res,next) => {
-    const role = req.user.role;
-    if(role != 'owner') return next(errorHandler(403, 'not an stadium owner'));
-    next();
-}
+exports.isAdmin = (req, res, next) => {
+  const adminId = req.user.admin_id;
+  if (!adminId) return next(errorHandler(403, "not admin system"));
+  next();
+};
 
+exports.isManager = (req, res, next) => {
+  const manager_id = req.user.manager_id;
+  if (!manager_id) return next(errorHandler(403, "not a stadium manager"));
+  next();
+};
 
-exports.isStaff = (req,res,next) => {
-    const role = req.user.role;
-    if(role != 'staff') return next(errorHandler(403, 'not a stadium staff'));
-    next();
-}
+exports.isOwner = (req, res, next) => {
+  const role = req.user.role;
+  if (role != "owner") return next(errorHandler(403, "not an stadium owner"));
+  next();
+};
 
-exports.isSelfPlayer = (req,res,next) => {
-    const playerId = req.params.playerId;
-    const currentPlayerId = req.user.player_id;
-    if(playerId != currentPlayerId) return next(errorHandler(403, 'not this player'));
-    next();
-}
+exports.isStaff = (req, res, next) => {
+  const role = req.user.role;
+  if (role != "staff") return next(errorHandler(403, "not a stadium staff"));
+  next();
+};
 
-exports.isSelfAdmin = (req,res,next) => {
-    const managerId = req.params.managerId;
-    const currentManagerId = req.user.manager_id;
-    if(managerId != currentManagerId) return next(errorHandler(403, 'not this manager'));
-    next();
-}
+exports.isSelfPlayer = (req, res, next) => {
+  const playerId = req.params.playerId;
+  const currentPlayerId = req.user.player_id;
+  if (playerId != currentPlayerId)
+    return next(errorHandler(403, "not this player"));
+  next();
+};
+
+exports.isSelfAdmin = (req, res, next) => {
+  const managerId = req.params.managerId;
+  const currentManagerId = req.user.manager_id;
+  if (managerId != currentManagerId)
+    return next(errorHandler(403, "not this manager"));
+  next();
+};
 
 //check if user is manager of this stadium
-exports.isManagerStadium = async (req,res,next) => {
-    const user = req.user;
-    const managerId = user.manager_id;
-    if(!managerId) return next(errorHandler(403, 'not a stadium manager'));
+exports.isManagerStadium = async (req, res, next) => {
+  const user = req.user;
+  const managerId = user.manager_id;
+  if (!managerId) return next(errorHandler(403, "not a stadium manager"));
 
-    const stadiumId = req.params.stadiumId;
-    const managers = await stadiumService.findManagerById(stadiumId);
-    if(!managers.includes(managerId)) return next(errorHandler(403, 'not this stadium manager'));
-    next();
-}
+  const stadiumId = req.params.stadiumId;
+  const managers = await stadiumService.findManagerById(stadiumId);
+  if (!managers.includes(managerId))
+    return next(errorHandler(403, "not this stadium manager"));
+  next();
+};
 
 //check if user is player own this order
-exports.isPlayerOrder = async (req,res,next) => {
-    const orderId = req.params.orderId;
-    const playerId = req.user.player_id;
+exports.isPlayerOrder = async (req, res, next) => {
+  const orderId = req.params.orderId;
+  const playerId = req.user.player_id;
 
-    const order  = await orderService.findOneOrder(orderId);
-    if(order.player_id != playerId) return next(errorHandler(401, 'player not own this order'));
-    next();
-}
+  const order = await orderService.findOneOrder(orderId);
+  if (order.player_id != playerId)
+    return next(errorHandler(401, "player not own this order"));
+  next();
+};
 
-exports.isManagerOrder = async(req,res,next) => {
-    const orderId = req.params.orderId;
-    const managerId = req.params.maanger_id;
+exports.isManagerOrder = async (req, res, next) => {
+  const orderId = req.params.orderId;
+  const managerId = req.params.maanger_id;
 
-    const orders = await orderService.findSelfOrdersByManager(managerId);
-    const orderIds = orders.map(order => order.id);
-    if(!orderIds.includes(orderId)) return next(errorHandler(401, 'manager not own this order'));
-    next();
-}
-
-
-
-
-
-
-
-
+  const orders = await orderService.findSelfOrdersByManager(managerId);
+  const orderIds = orders.map((order) => order.id);
+  if (!orderIds.includes(orderId))
+    return next(errorHandler(401, "manager not own this order"));
+  next();
+};
